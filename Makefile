ba9ea7faeab9f69462f5080e21fa75c3a670953d
Joe Gregorio <jcgregorio@google.com>
2010-08-19 15:49:04 -0400

    Added tests for resources and their methods. Also added tests for the json model.

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 pep8:
 	find apiclient samples -name "*.py" | xargs pep8 --ignore=E111,E202
+
+test:
+	python runtests.py
---
+++ tests/test_json_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python2.4
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+
+"""Discovery document tests
+
+Unit tests for objects created from discovery documents.
+"""
+
+__author__ = 'jcgregorio@google.com (Joe Gregorio)'
+
+from apiclient.discovery import JsonModel
+import os
+import unittest
+
+
+class Model(unittest.TestCase):
+  def test_json_no_body(self):
+    model = JsonModel()
+
+    headers = {}
+    params = {}
+
+    headers, params, query, body = model.request(headers, params)
+
+    self.assertEqual(headers['accept'], 'application/json')
+    self.assertTrue('content-type' not in headers)
+    self.assertNotEqual(query, '')
+    self.assertEqual(body, None)
+
+  def test_json_body(self):
+    model = JsonModel()
+
+    headers = {}
+    params = {'body': {}}
+
+    headers, params, query, body = model.request(headers, params)
+
+    self.assertEqual(headers['accept'], 'application/json')
+    self.assertEqual(headers['content-type'], 'application/json')
+    self.assertNotEqual(query, '')
+    self.assertEqual(body, '{"data": {}}')
+
+if __name__ == '__main__':
+  unittest.main()
