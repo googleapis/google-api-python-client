c63823a7774daa134dd14c8e1a090d78be830f3f
Joe Gregorio <jcgregorio@google.com>
2010-10-15 10:41:12 -0400

    Move django utilities from sample into .ext.

---
+++ apiclient/ext/django.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class OAuthCredentialsField(models.Field):
+
+  __metaclass__ = models.SubfieldBase
+
+  def db_type(self):
+    return 'VARCHAR'
+
+  def to_python(self, value):
+    if value is None:
+      return None
+    if isinstance(value, apiclient.oauth.Credentials):
+      return value
+    return pickle.loads(base64.b64decode(value))
+
+  def get_db_prep_value(self, value):
+    return base64.b64encode(pickle.dumps(value))
+
+class FlowThreeLeggedField(models.Field):
+
+  __metaclass__ = models.SubfieldBase
+
+  def db_type(self):
+    return 'VARCHAR'
+
+  def to_python(self, value):
+    print "In to_python", value
+    if value is None:
+      return None
+    if isinstance(value, apiclient.oauth.FlowThreeLegged):
+      return value
+    return pickle.loads(base64.b64decode(value))
+
+  def get_db_prep_value(self, value):
+    return base64.b64encode(pickle.dumps(value))
