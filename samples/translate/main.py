618e63b11b7978e34d65d84452258251b8ab9e5b
Joe Gregorio <jcgregorio@google.com>
2010-11-04 19:55:28 -0400

    Added sample for translate v2 api

---
+++ samples/translate/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2.4
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+
+"""Simple command-line example for Translate.
+
+Command-line application that translates
+some text.
+"""
+
+__author__ = 'jcgregorio@google.com (Joe Gregorio)'
+
+from apiclient.discovery import build
+
+import pprint
+
+# Uncomment the next line to get very detailed logging
+# httplib2.debuglevel = 4
+
+def main():
+
+  p = build("translate", "v2", developerKey="AIzaSyDRRpR3GS1F1_jKNNM9HCNd2wJQyPG3oN0")
+  print p.translations().list(
+      source="en",
+      target="fr",
+      q="flower"
+    ).execute()
+
+if __name__ == '__main__':
+  main()
