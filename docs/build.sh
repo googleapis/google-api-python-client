55d58180edce947b23559c5b650d0eb2bb554899
Joe Gregorio <jcgregorio@google.com>
2011-02-20 01:42:07 -0500

    Added httplib2 and uritemplate to generated docs

--- docs/build.sh
+++ docs/build.sh
@@ -14,4 +14,6 @@ export DJANGO_SETTINGS_MODULE=fakesettings
 export PYTHONPATH=`pwd`/..:$GOOGLE_APPENGINE
 find ../apiclient/ -name "*.py" | sed "s/\/__init__.py//" | sed "s/\.py//" | sed "s/^\.\.\///" | sed "s#/#.#g" | xargs pydoc -w
 find ../oauth2client/ -name "*.py" | sed "s/\/__init__.py//" | sed "s/\.py//" | sed "s/^\.\.\///" | sed "s#/#.#g" | xargs pydoc -w
+find ../httplib2/ -name "*.py" | sed "s/\/__init__.py//" | sed "s/\.py//" | sed "s/^\.\.\///" | sed "s#/#.#g" | xargs pydoc -w
+find ../uritemplate/ -name "*.py" | sed "s/\/__init__.py//" | sed "s/\.py//" | sed "s/^\.\.\///" | sed "s#/#.#g" | xargs pydoc -w
 
