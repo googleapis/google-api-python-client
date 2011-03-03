93841708c56f7a1ec080ff5f113cf0c4d7d0eb2a
Joe Gregorio <jcgregorio@google.com>
2011-03-02 16:12:05 -0800

    Add checks to the release target for confirmation. Also run tests before building a release. Refresh docs.

--- Makefile
+++ Makefile
@@ -11,12 +11,17 @@ docs:
 
 .PHONY: prerelease
 prerelease:
+	python runtests.py --exit_on_failure tests
 	-rm dist/*
 	python setup.py clean
 	python setup.py sdist
 
 .PHONY: release
 release: prerelease
+	@echo "This target will upload a new release to PyPi and code.google.com hosting."
+	@echo "Are you sure you want to proceed? (yes/no)"
+	@read yn; [ "yes" == $$yn ]
+	@echo "Here we go..."
 	python setup.py sdist register upload
 	wget "http://support.googlecode.com/svn/trunk/scripts/googlecode_upload.py" -O googlecode_upload.py
 	python googlecode_upload.py --summary="Version $(shell python setup.py --version)" --project=google-api-python-client dist/*.tar.gz
--- runtests.py
+++ runtests.py
@@ -35,17 +35,20 @@ def build_suite(folder, verbosity):
       __import__(module)
     raise
 
-def run(test_folder_name, verbosity=1):
+def run(test_folder_name, verbosity, exit_on_failure):
   # Build and run the tests in test_folder_name
   tests = build_suite(test_folder_name, verbosity)
-  unittest.TextTestRunner(verbosity=verbosity).run(tests)
+  result = unittest.TextTestRunner(verbosity=verbosity).run(tests)
+  if exit_on_failure and not result.wasSuccessful():
+    sys.exit(1)
   cleanup()
 
 def main():
   if '--help' in sys.argv:
-    print 'Usage: python runtests.py [--q|-quiet|--v|-verbose] [tests|functional_tests|contrib_tests]'
+    print 'Usage: python runtests.py [-q|--quiet|-v|--verbose] [--exit_on_failure] [tests|functional_tests|contrib_tests]'
     return
   verbosity = 1
+  exit_on_failure = '--exit_on_failure' in sys.argv
   if '-q' in sys.argv or '--quiet' in sys.argv:
     verbosity = 0
   if "-v" in sys.argv or '--verbose' in sys.argv:
@@ -59,15 +62,15 @@ def main():
 
   # Allow user to run a specific folder of tests
   if 'tests' in sys.argv:
-    run('tests', verbosity)
+    run('tests', verbosity, exit_on_failure)
   elif 'functional_tests' in sys.argv:
-    run('functional_tests', verbosity)
+    run('functional_tests', verbosity, exit_on_failure)
   elif 'contrib_tests' in sys.argv:
-    run('contrib_tests', verbosity)
+    run('contrib_tests', verbosity, exit_on_failure)
   else:
-    run('tests', verbosity)
-    run('functional_tests', verbosity)
-    run('contrib_tests', verbosity)
+    run('tests', verbosity, exit_on_failure)
+    run('functional_tests', verbosity, exit_on_failure)
+    run('contrib_tests', verbosity, exit_on_failure)
 
 if __name__ == '__main__':
   main()
