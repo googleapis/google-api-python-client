eb8666f6ffcf4cdc0d56ef51fc1cc96e567f1422
Florent Espanet <florent@lumapps.com>
2015-09-08 16:44:20 +0200

    Add ParameterRequiredError
    When an endpoint's path parameter is not in the input message it should
    raise this exception.

--- googleapiclient/errors.py
+++ googleapiclient/errors.py
@@ -107,6 +107,10 @@ class InvalidNotificationError(Error):
   """The channel Notification is invalid."""
   pass
 
+class ParameterRequiredError(Error):
+  """The url parameter is not present in the input message."""
+  pass
+
 class BatchError(HttpError):
   """Error occured during batch operations."""
 
