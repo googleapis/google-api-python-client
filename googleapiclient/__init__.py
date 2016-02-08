d702530286051e473ad1f50e7fd724b86207df06
Emmett Butler <emmett.butler321@gmail.com>
2016-02-08 14:26:00 -0800

    move logging into __name__-specific loggers from the root logger

--- googleapiclient/__init__.py
+++ googleapiclient/__init__.py
@@ -14,3 +14,14 @@
 
 __version__ = "1.5.0"
 
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
