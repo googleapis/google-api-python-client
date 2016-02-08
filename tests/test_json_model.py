d90c03d25e933fb38f260fab30194eaa47df0390
Emmett Butler <emmett.butler321@gmail.com>
2016-02-08 14:26:00 -0800

    move logging into __name__-specific loggers from the root logger

--- tests/test_json_model.py
+++ tests/test_json_model.py
@@ -219,8 +219,8 @@ class Model(unittest.TestCase):
         self.status = items['status']
         for key, value in six.iteritems(items):
           self[key] = value
-    old_logging = googleapiclient.model.logging
-    googleapiclient.model.logging = MockLogging()
+    old_logging = googleapiclient.model.LOGGER
+    googleapiclient.model.LOGGER = MockLogging()
     googleapiclient.model.dump_request_response = True
     model = JsonModel()
     request_body = {
@@ -236,18 +236,18 @@ class Model(unittest.TestCase):
                 'response_field_2': 'response_value_2'}
     response_body = model.response(MockResponse(response), body_string)
     self.assertEqual(request_body, response_body)
-    self.assertEqual(googleapiclient.model.logging.info_record[:2],
+    self.assertEqual(googleapiclient.model.LOGGER.info_record[:2],
                      ['--request-start--',
                       '-headers-start-'])
     self.assertTrue('response_field_1: response_value_1' in
-                    googleapiclient.model.logging.info_record)
+                    googleapiclient.model.LOGGER.info_record)
     self.assertTrue('response_field_2: response_value_2' in
-                    googleapiclient.model.logging.info_record)
-    self.assertEqual(json.loads(googleapiclient.model.logging.info_record[-2]),
+                    googleapiclient.model.LOGGER.info_record)
+    self.assertEqual(json.loads(googleapiclient.model.LOGGER.info_record[-2]),
                      request_body)
-    self.assertEqual(googleapiclient.model.logging.info_record[-1],
+    self.assertEqual(googleapiclient.model.LOGGER.info_record[-1],
                      '--response-end--')
-    googleapiclient.model.logging = old_logging
+    googleapiclient.model.LOGGER = old_logging
 
   def test_no_data_wrapper_deserialize(self):
     model = JsonModel(data_wrapper=False)
