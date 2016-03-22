a9ffde78f5eab80434f0926738fb591a23d42516
Emmett Butler <emmett.butler321@gmail.com>
2016-03-22 08:21:56 -0700

    add nullhandler to __init__

--- googleapiclient/__init__.py
+++ googleapiclient/__init__.py
@@ -13,3 +13,12 @@
 # limitations under the License.
 
 __version__ = "1.5.0"
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
