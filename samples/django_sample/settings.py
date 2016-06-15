83c8a7abaab31f878eaa84d25e5c6fd224cbc63b
kunal95 <kunsachdeva@gmail.com>
2016-06-15 21:05:17 +0530

    TEMPLATE_DIRS updated

--- samples/django_sample/settings.py
+++ samples/django_sample/settings.py
@@ -66,12 +66,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'django_sample.urls'
 
-TEMPLATE_DIRS = (
+TEMPLATE_DIRS = [
     # Put strings here, like "/home/html/django_templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(__file__), 'templates')
-)
+]
 
 INSTALLED_APPS = (
     'django.contrib.admin',
