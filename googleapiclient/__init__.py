db5a0ed106aff656a031a99ad64780ffd88c8425
Jon Wayne Parrott <jonwayne@google.com>
2016-10-17 15:39:10 -0700

    Update version to 1.5.4

--- googleapiclient/__init__.py
+++ googleapiclient/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.5.3"
+__version__ = "1.5.4"
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
