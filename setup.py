afe134b57f8b128308739e62957e5b796e0bc1e9
Jon Wayne Parrott <jonwayne@google.com>
2017-02-08 10:20:57 -0800

    Release v1.6.2 (#348)

--- setup.py
+++ setup.py
@@ -93,6 +93,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
