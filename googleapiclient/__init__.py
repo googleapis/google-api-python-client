27d68a89e2515fb55157768c9143d7436ffb5116
Jon Wayne Parrott <jonwayne@google.com>
2017-02-08 10:17:59 -0800

    Release v1.6.2

--- googleapiclient/__init__.py
+++ googleapiclient/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
