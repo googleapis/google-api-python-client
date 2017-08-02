5a8d7c61771ea62d0dd3189896c69e8cd6771316
Jon Wayne Parrott <jonwayne@google.com>
2017-08-02 12:58:23 -0700

    Don't treat httplib2.Credentials as oauth credentials

--- googleapiclient/_auth.py
+++ googleapiclient/_auth.py
@@ -126,7 +126,8 @@ def get_credentials_from_http(http):
         return None
     elif hasattr(http.request, 'credentials'):
         return http.request.credentials
-    elif hasattr(http, 'credentials'):
+    elif (hasattr(http, 'credentials')
+          and not isinstance(http.credentials, httplib2.Credentials)):
         return http.credentials
     else:
         return None
