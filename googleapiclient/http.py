da7de37ce3fdaaf036ef72d9c7a2de110b705e9e
Nilayan Bhattacharya <nilayan.bhatt@gmail.com>
2017-12-04 14:02:43 -0800

    Handling error returned as a list

--- googleapiclient/http.py
+++ googleapiclient/http.py
@@ -89,7 +89,7 @@ def _should_retry_response(resp_status, content):
 
   Args:
     resp_status: The response status received.
-    content: The response content body. 
+    content: The response content body.
 
   Returns:
     True if the response should be retried, otherwise False.
@@ -112,7 +112,10 @@ def _should_retry_response(resp_status, content):
     # Content is in JSON format.
     try:
       data = json.loads(content.decode('utf-8'))
-      reason = data['error']['errors'][0]['reason']
+      if isinstance(data, dict):
+        reason = data['error']['errors'][0]['reason']
+      else:
+        reason = data[0]['error']['errors']['reason']
     except (UnicodeDecodeError, ValueError, KeyError):
       LOGGER.warning('Invalid JSON content from response: %s', content)
       return False
