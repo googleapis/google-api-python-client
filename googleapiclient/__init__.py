081d2a336393a5d497339e462875294cfd8c81ea
Thea Flowers <theaflowers@google.com>
2018-06-01 10:22:59 -0700

    Release v1.7.1 (#519)

--- googleapiclient/__init__.py
+++ googleapiclient/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
