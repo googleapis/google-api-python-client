f38ad863f7dfaabda03b919fcc5191b19393ae27
Benamar Belarbi <benamarbelarbi@yahoo.fr>
2018-11-09 18:13:48 +0100

    fix issue #586 : cache should handle appengine memcache 1000000 bytes limitation

--- googleapiclient/discovery_cache/appengine_memcache.py
+++ googleapiclient/discovery_cache/appengine_memcache.py
@@ -15,6 +15,7 @@
 """App Engine memcache based cache for the discovery document."""
 
 import logging
+import pickle
 
 # This is only an optional dependency because we only import this
 # module when google.appengine.api.memcache is available.
@@ -32,23 +33,49 @@ NAMESPACE = 'google-api-client'
 class Cache(base.Cache):
   """A cache with app engine memcache API."""
 
-  def __init__(self, max_age):
+  def __init__(self, max_age, use_multi=True):
       """Constructor.
 
       Args:
         max_age: Cache expiration in seconds.
+        use_multi: use multi keys cache to overcome MAX size limitation.
       """
       self._max_age = max_age
+      self._use_multi = use_multi
 
   def get(self, url):
+    if self._use_multi:
+      return self.get_multi(url)
+
     try:
       return memcache.get(url, namespace=NAMESPACE)
     except Exception as e:
       LOGGER.warning(e, exc_info=True)
 
   def set(self, url, content):
+    if self._use_multi:
+      return self.set_multi(url,content)
+
     try:
       memcache.set(url, content, time=int(self._max_age), namespace=NAMESPACE)
+    except Exception as e:
+      LOGGER.warning(e, exc_info=True)
+
+  def set_multi(self, key, value, chunksize=950000):
+    try:
+      serialized = pickle.dumps(value, 2)
+      values = {}
+      for i in xrange(0, len(serialized), chunksize):
+        values['%s.%s' % (key, i//chunksize)] = serialized[i : i+chunksize]
+      memcache.set_multi(values)
+    except Exception as e:
+      LOGGER.warning(e, exc_info=True)
+
+  def get_multi(self, key):
+    try:
+      result = memcache.get_multi(['%s.%s' % (key, i) for i in xrange(32)])
+      serialized = ''.join([v for k, v in sorted(result.items()) if v is not None])
+      return pickle.loads(serialized)
     except Exception as e:
       LOGGER.warning(e, exc_info=True)
 
