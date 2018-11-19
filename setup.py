6bdd738cb7b370ae9f844496423ad74376ff4d6d
Bu Sun Kim <busunkim@google.com>
2018-11-19 15:23:26 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
