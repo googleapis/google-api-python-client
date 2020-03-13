a480d53d84d16938d46b67df2c3e9bf9f49642c6
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2020-03-13 12:52:22 -0700

    fix: don't set http.redirect_codes if the attr doesn't exist and allow more httplib2 versions (#841)
    * fix: don't set http.redirect_codes if the attr doesn't exist
    * fix: widen permitted httplib2 versions

--- setup.py
+++ setup.py
@@ -33,7 +33,10 @@ from setuptools import setup
 packages = ["apiclient", "googleapiclient", "googleapiclient/discovery_cache"]
 
 install_requires = [
-    "httplib2>=0.17.0,<1dev",
+    # NOTE: Apache Beam tests depend on this library and cannot
+    # currently upgrade their httplib2 version.
+    # Please see https://github.com/googleapis/google-api-python-client/pull/841
+    "httplib2>=0.9.2,<1dev",
     "google-auth>=1.4.1",
     "google-auth-httplib2>=0.0.3",
     "google-api-core>=1.13.0,<2dev",
