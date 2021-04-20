274f1c86d4432e6ac68bcff17a90d0aebbb8cc9d
Anthonios Partheniou <partheniou@google.com>
2021-04-20 13:17:26 +0000

    Revert "Test remove fix from #1221"
    This reverts commit 4ad06e60cc1f43f68cf32e4f8a97bb59012312d6.

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setuptools.setup(
     install_requires=install_requires,
     python_requires=">=3.6",
     packages=packages,
-    package_data={},
+    package_data={"googleapiclient": ["discovery_cache/documents/*.json"]},
     license="Apache 2.0",
     keywords="google api client",
     classifiers=[
