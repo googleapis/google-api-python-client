ac19c55201b5f1b7ef2564aa373b4c5f09d85bc6
Anthonios Partheniou <partheniou@google.com>
2021-04-25 16:03:21 +0000

    Increase coverage for scripts folder to 91%

--- noxfile.py
+++ noxfile.py
@@ -105,7 +105,7 @@ def scripts(session):
         "--cov=scripts",
         "--cov-config=.coveragerc",
         "--cov-report=",
-        "--cov-fail-under=80",
+        "--cov-fail-under=91",
         "scripts",
         *session.posargs,
     )
