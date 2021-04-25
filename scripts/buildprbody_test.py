36c8ada757b3de1b69ae4894ff11a26399b54b9e
Anthonios Partheniou <partheniou@google.com>
2021-04-25 16:09:32 +0000

    Fix typo

--- scripts/buildprbody_test.py
+++ scripts/buildprbody_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""ChangeSummary tests."""
+"""BuildPrBody tests."""
 
 __author__ = "partheniou@google.com (Anthonios Partheniou)"
 
