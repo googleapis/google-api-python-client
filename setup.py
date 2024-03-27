da0ef96377af42b9da294ddd5d18b8ed3d147dfa
ohmayr <omairnaveed@ymail.com>
2024-03-27 17:26:36 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ install_requires = [
     # NOTE: Maintainers, please do not require google-auth>=2.x.x
     # Until this issue is closed
     # https://github.com/googleapis/google-cloud-python/issues/10566
-    "google-auth>=1.32.0,<3.0.0.dev0", "!=2.24.0", "!=2.25.0"
+    "google-auth>=1.32.0,<3.0.0.dev0,!=2.24.0,!=2.25.0",
     "google-auth-httplib2>=0.2.0",
     # NOTE: Maintainers, please do not require google-api-core>=2.x.x
     # Until this issue is closed
