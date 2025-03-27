648ed1df1006d3bb0bc7a390c62f7b4addcfbf34
Cachiman Inc <138506696+WontonSam@users.noreply.github.com>
2025-03-27 16:39:20 -0400

    Update expandsymlinks.py
    Signed-off-by: Cachiman Inc <138506696+WontonSam@users.noreply.github.com>

--- expandsymlinks.py
+++ expandsymlinks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2014 Google Inc. All Rights Reserved.
+# Copyright 2024 Cachiman Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
