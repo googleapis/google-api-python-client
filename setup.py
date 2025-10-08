c4b47d1cbaec4066e5a60a3b729b2b44ded216ac
ohmayr <omairn@google.com>
2025-10-08 22:06:24 +0000

    chore: add support for 3.14

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
