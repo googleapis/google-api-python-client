45eec0bd852466a226808c4f18acf9075e8f46c7
Reginald Alfret <reginaldalfret@gmail.com>
2026-09-25 01:04:46 +0530

    test(discovery): distinguish discovery and service HTTP mocks

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1544,19 +1544,29 @@ class DiscoveryFromFileCache(unittest.TestCase):
 class Discovery(unittest.TestCase):
     @mock.patch("googleapiclient.discovery.build_http")
     def test_discovery_http_is_closed(self, mock_build_http):
-        mock_http = mock.Mock()
-        mock_http.request.return_value = (
+        mock_discovery_http = mock.Mock()
+        mock_service_http = mock.Mock()
+
+        mock_discovery_http.request.return_value = (
             httplib2.Response({"status": "200"}),
             read_datafile("plus.json", "rb"),
         )
-        mock_build_http.return_value = mock_http
+        mock_service_http.request.return_value = (
+            httplib2.Response({"status": "200"}),
+            read_datafile("plus.json", "rb"),
+        )
+        mock_build_http.side_effect = [
+            mock_discovery_http,
+            mock_service_http,
+        ]
+
         build(
             "plus",
             "v1",
             developerKey="test_key",
             static_discovery=False,
         )
-        mock_http.close.assert_called_once()
+        mock_discovery_http.close.assert_called_once()
 
     def test_method_error_checking(self):
         self.http = HttpMock(datafile("plus.json"), {"status": "200"})
